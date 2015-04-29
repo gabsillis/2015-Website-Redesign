3488fef981d6a8a241e73493824e04d9a9edf6e4
Gianni <gianniabsillis@gmail.com>
2015-04-29 16:59:29 -0400

    initial commit

---
+++ wp-content/themes/RoboTheme/index.php
@@ -0,0 +1,34 @@
+<html>
+    <?php
+        /*
+            Template Name: My Custom Page
+        */
+        /*
+        IDEAS:
+        Array to loop through and generate navigation
+        */
+    ?>
+    <head>
+        <?php
+            echo '<title>' + bloginfo('name') + '</title>';
+        ?>
+        <style type="text/css"/>
+            @import url("style.css");
+        </style>
+    </head>
+    <body>
+        <nav>
+            <ul>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><p>Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><p>More Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><p>More Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><p>More Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><p>More Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><p>More Text</p></li>
+            </ul>
+        </nav>
+        <?php
+            
+        ?>
+    </body>
+</html>
