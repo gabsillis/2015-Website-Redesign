39ee5d71dded60bac3ce26dbe3fefa9204c1b0e0
Gianni <gianniabsillis@gmail.com>
2015-05-29 20:41:54 -0400

    started navigation

---
+++ index.php
@@ -0,0 +1,35 @@
+<html>
+    <?php
+        /*
+            Template Name: My Custom Page
+        */
+        /*
+        IDEAS:
+        Array to loop through and generate navigation
+        */
+    ?>
+    <head>
+        <?php
+            echo '<title>' + bloginfo('name') + '</title>';
+        ?>
+        <style type="text/css"/>
+            @import url("style.css");
+        </style>
+    </head>
+    <body>
+    <div class="header"><h1 style="color:#001D55;">Team435</h1><h1 style="color:#1F5C1F;"> Robodogs</h1></div>
+        <nav>
+            <ul>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
+            </ul>
+        </nav>
+        <?php
+            
+        ?>
+    </body>
+</html>
