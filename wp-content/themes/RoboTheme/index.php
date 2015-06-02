1f691cb8178871ab61957753de36c2ff5085fedc
Gianni <gianniabsillis@gmail.com>
2015-06-01 22:26:23 -0400

    polished new layout

--- wp-content/themes/RoboTheme/index.php
+++ wp-content/themes/RoboTheme/index.php
@@ -18,10 +18,10 @@
     </head>
     <body>
 	<div class = "header_container">
-		<div class="header"><h1 style="color:#001D55;">Team435</h1><h1 style="color:#1F5C1F;"> Robodogs</h1></div>
+		<div class="header"><h1 style="color:#001D55;">Team435</h1><h1 class="commodoreFont" style="color:#1F5C1F;"> Robodogs</h1></div>
 			<nav>
 				<ul>
-					<li><a href="index.php"><img src="home-icon.svg" onerror="this.onerror=null; this.src='image.png'"><div></div></a></li>
+					<li><a href="index.php"><img src="home-icon.svg" onerror="this.onerror=null; this.src='image.png'"></a></li>
 					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
 					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
 					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
@@ -29,6 +29,7 @@
 					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
 				</ul>
 			</nav>
+			<div class="under_accent" style="background-color: #a9a29c;"></div>
 	</div>
         <?php
             
