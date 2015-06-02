8fcc747942708c0d007d939545e524933a6eba2d
Gianni <gianniabsillis@gmail.com>
2015-06-01 21:29:51 -0400

    Worked on Navigation

--- wp-content/themes/RoboTheme/index.php
+++ wp-content/themes/RoboTheme/index.php
@@ -20,12 +20,12 @@
     <div class="header"><h1 style="color:#001D55;">Team435</h1><h1 style="color:#1F5C1F;"> Robodogs</h1></div>
         <nav>
             <ul>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>Text</p></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div><p>More Text</p></li>
+                <li><a href="index.php"><img src="home-icon.svg" onerror="this.onerror=null; this.src='image.png'"><div></div></a></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
             </ul>
         </nav>
         <?php
