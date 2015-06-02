1e673a30eace52dc546a1a1fbd17063429d16e11
Gianni <gianniabsillis@gmail.com>
2015-06-01 21:56:45 -0400

    redid nav and logo layout

--- wp-content/themes/RoboTheme/index.php
+++ wp-content/themes/RoboTheme/index.php
@@ -17,17 +17,19 @@
         </style>
     </head>
     <body>
-    <div class="header"><h1 style="color:#001D55;">Team435</h1><h1 style="color:#1F5C1F;"> Robodogs</h1></div>
-        <nav>
-            <ul>
-                <li><a href="index.php"><img src="home-icon.svg" onerror="this.onerror=null; this.src='image.png'"><div></div></a></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
-                <li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
-            </ul>
-        </nav>
+	<div class = "header_container">
+		<div class="header"><h1 style="color:#001D55;">Team435</h1><h1 style="color:#1F5C1F;"> Robodogs</h1></div>
+			<nav>
+				<ul>
+					<li><a href="index.php"><img src="home-icon.svg" onerror="this.onerror=null; this.src='image.png'"><div></div></a></li>
+					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+				</ul>
+			</nav>
+	</div>
         <?php
             
         ?>
