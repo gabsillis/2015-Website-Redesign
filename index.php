5dd4f7ccc36c1fdcc25fe772ca60b4db1f36d04f
czvni <czvnia@gmail.com>
2015-07-05 19:45:54 -0300

    new pages

---
+++ index.php
@@ -0,0 +1,44 @@
+<html>
+    <?php
+        /*
+            Template Name: My Custom Page
+        */
+        /*
+        IDEAS:
+        Array to loop through and generate navigation
+        */
+    ?>
+    <head>
+        <?php
+		
+            
+			
+        ?>
+        <style type="text/css"/>
+            @import url("style.css");
+        </style>
+    </head>
+    <body>
+	<div class = "header_container">
+		<div class="header"><h1 style="color:#001D55;">Team435</h1><h1 class="commodoreFont" style="color:#1F5C1F;"> Robodogs</h1></div>
+			<nav>
+				<ul>
+					<li><a href="index.php"><img src="home-icon.svg" onerror="this.onerror=null; this.src='image.png'"><div></div></a></li>
+					<li><a href="divisions.php"><img src="divisions-icon.svg"/></a></li>
+					<li><a href="newmembers.php"><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></a></li>
+					<li><a href="members.php"><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></a></li>
+					<li><a href="robot.php"><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></a></li>
+					<li><a href="contact.php"><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></a></li>
+				</ul>
+			</nav>
+			<div class="under_accent" style="background-color: #a9a29c;"></div>
+	</div>
+	<div class = "slideshow_container">
+		<img src="old_logo.gif" class="slideshow_visible"/>
+		<div id="caption"></div>
+	</div>
+        <?php
+            
+        ?>
+    </body>
+</html>
