960122cfa95a3a1ecce09cce54160d9fcc51c78e
czvni <czvnia@gmail.com>
2015-07-05 17:09:59 -0300

    Added pages under <ul>

--- wp-content/themes/RoboTheme/index.php
+++ wp-content/themes/RoboTheme/index.php
@@ -23,12 +23,12 @@
 		<div class="header"><h1 style="color:#001D55;">Team435</h1><h1 class="commodoreFont" style="color:#1F5C1F;"> Robodogs</h1></div>
 			<nav>
 				<ul>
-					<li><a href="index.php"><img src="home-icon.svg" onerror="this.onerror=null; this.src='image.png'"></a></li>
-					<li><a href="divisions.php"><img src="divisions-icon.svg"/><div></div></a></li>
-					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
-					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
-					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
-					<li><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></li>
+					<li><a href="index.php"><img src="home-icon.svg" onerror="this.onerror=null; this.src='image.png'"><div></div></a></li>
+					<li><a href="divisions.php"><img src="divisions-icon.svg"/></a></li>
+					<li><a href="newmembers.php"><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></a></li>
+					<li><a href="members.php"><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></a></li>
+					<li><a href="robot.php"><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></a></li>
+					<li><a href="contact.php"><img src="http://www.devsourcecodex.com/images/advertisingexamples/200x200.png"/><div></div></a></li>
 				</ul>
 			</nav>
 			<div class="under_accent" style="background-color: #a9a29c;"></div>
