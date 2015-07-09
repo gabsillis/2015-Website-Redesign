da6048164c0854a3fe6b02591d5de69d4df80b08
Gabriel Adamson <adamson.g@gmail.com>
2015-07-09 15:43:30 -0400

    Made improvements to the members page

--- members.php
+++ members.php
@@ -14,9 +14,10 @@ a {
 }
 </style>
 <br />
+<object style="border: solid; margin: 8px 2px; position: absolute; right: 0px; top: 195" data="http://www.google.com/calendar/embed?title=Public%20Calendar&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;src=robodogs.org_mjjoqm31ubpi5e9hinbn1bhsbc%40group.calendar.google.com&amp;ctz=America%2FNew_York" width="650" height="600"><a onclick="window.open(this.href,'_blank');return false;" href="http://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;src=robodogs.org_mjjoqm31ubpi5e9hinbn1bhsbc%40group.calendar.google.com&amp;ctz=America%2FNew_York">Public Calendar</a></object>
+<div id="left-pane">
 Current members: Please check the <a href="http://robodogs.org/forums">forums</a> <u>daily</u>.<br />
 The calendar may be used as a suppliment to, <b><u>not</u></b> a replacement for, the forums.
-<object style="border: solid; margin: 8px 2px; position: absolute; right: 0px; top: 195" data="http://www.google.com/calendar/embed?title=Public%20Calendar&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;src=robodogs.org_mjjoqm31ubpi5e9hinbn1bhsbc%40group.calendar.google.com&amp;ctz=America%2FNew_York" width="650" height="600"><a onclick="window.open(this.href,'_blank');return false;" href="http://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;src=robodogs.org_mjjoqm31ubpi5e9hinbn1bhsbc%40group.calendar.google.com&amp;ctz=America%2FNew_York">Public Calendar</a></object>
 <?php $args = array(
 	'posts_per_page'   => 5000,
 	'category'         => 2
@@ -31,4 +32,5 @@ $posts_array = get_posts( $args );
 <?php endfor; ?>
 </div>
 <h2>Tonight's dinner is pizza.</h2>
+</div>
 </div>
