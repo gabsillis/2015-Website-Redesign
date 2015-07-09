7abc2a195c105bb56e14e867ef02b44893a96622
Gabriel Adamson <adamson.g@gmail.com>
2015-07-09 19:23:33 -0400

    I forgot to change this in the file, should have been in last pull request

--- members.php
+++ members.php
@@ -12,9 +12,14 @@ a {
 	text-decoration: none;
 	color: #1F5C1F;
 }
+#left-pane {
+	width: 50%;
+	background-color: #FBFBFB;
+	border: solid;
+}
 </style>
 <br />
-<object style="border: solid; margin: 8px 2px; position: absolute; right: 0px; top: 195" data="http://www.google.com/calendar/embed?title=Public%20Calendar&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;src=robodogs.org_mjjoqm31ubpi5e9hinbn1bhsbc%40group.calendar.google.com&amp;ctz=America%2FNew_York" width="650" height="600"><a onclick="window.open(this.href,'_blank');return false;" href="http://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;src=robodogs.org_mjjoqm31ubpi5e9hinbn1bhsbc%40group.calendar.google.com&amp;ctz=America%2FNew_York">Public Calendar</a></object>
+<object style="border: solid; margin: 8px 2px; position: absolute; right: 0px; top: 203" data="http://www.google.com/calendar/embed?title=Public%20Calendar&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;src=robodogs.org_mjjoqm31ubpi5e9hinbn1bhsbc%40group.calendar.google.com&amp;ctz=America%2FNew_York" width="650" height="600"><a onclick="window.open(this.href,'_blank');return false;" href="http://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;src=robodogs.org_mjjoqm31ubpi5e9hinbn1bhsbc%40group.calendar.google.com&amp;ctz=America%2FNew_York">Public Calendar</a></object>
 <div id="left-pane">
 Current members: Please check the <a href="http://robodogs.org/forums">forums</a> <u>daily</u>.<br />
 The calendar may be used as a suppliment to, <b><u>not</u></b> a replacement for, the forums.
